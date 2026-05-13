761063840589854e83a4ea0f62f910ba1e6cdc69
zaid <shahwanzaid21@gmail.com>
2026-05-13 11:37:45 +0300

    Family_member modified

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::prefix('v1')->group(function()
             Route::post('/nurse/register', [RegistrationController::class, 'nurseRegister'])->middleware(['account.type:nurse']);
             Route::post('/companion/register', [RegistrationController::class, 'companionRegister'])->middleware(['account.type:companion']);
             Route::post('/driver/register', [RegistrationController::class, 'driverRegister'])->middleware(['account.type:driver']);
-            Route::post('/family-member/register', [RegistrationController::class, 'familyMemberRegister'])->middleware(['account.type:family-member']);
+            Route::post('/family_member/register', [RegistrationController::class, 'familyMemberRegister'])->middleware(['account.type:family_member']);
             Route::post('/elder/register', [RegistrationController::class, 'elderRegister'])->middleware(['account.type:elderly']);
             
             Route::get('/me', [LoginController::class, 'me']);
